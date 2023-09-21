a54a642b3ce600414cac4bb66b27b82e5f7e4450
Danila Bublik <dbublik11@gmail.com>
2023-09-21 13:37:53 +1000

    Add button highlight

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,36 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 
 export default function HeaderCartButton(props) {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartContext = useContext(CartContext);
+    const {items} = cartContext;
 
-    const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer)
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
